25e4aff5940b737eb97c574e6232c88f0f166fce
nehacp <neha.chaudhary@hotmail.com>
2017-09-20 22:17:57 -0700

    Add username and userId property to session object

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,19 +8,54 @@ module.exports.createSession = (req, res, next) => {
     .then(session => {
       req.session = { hash: session.hash };
       res.cookie('shortlyid', session.hash);
-      next();  
+    }).then(() => models.Users.get({username: req.body.username}))
+    .then((userInfo) => {
+      if (userInfo) {
+        req.session.user = {
+          username: userInfo.username
+        };
+        req.session.userId = userInfo.id;
+        next();
+      } else {
+        next();
+      }
     })
     .catch((err) => {
       console.log('Error creating a session', err);
       next();  
     });
   } else {
+    //console.log('here with cookie', req.body.username);
     req.session = {
       hash: req.cookies.shortlyid
     };
-    next();
+    models.Sessions.get({hash: req.cookies.shortlyid})
+    .then((result) => {
+      if (result) {
+        req.session.userId = result.userId;
+        models.Users.get({id: result.userId})
+        .then((userInfo) => {
+          if (userInfo) {
+            req.session.user = {
+              username: userInfo.username
+            };
+            next();
+          } else {
+            next();
+          }
+        });
+      } else {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log('Error creating a session', err);
+      next();  
+    });
   }
 };
+
+
 
 // module.exports.assignSession = (req, res, next) => {
 //   console.log('cookies in assign', req.cookies);
